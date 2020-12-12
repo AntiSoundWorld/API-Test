11301efc4b43c0cab06af7f52311a16f63de99fc
AntiSoundWorld <antisoundworld@gmail.com>
2020-12-12 19:09:58 +0300

    test

--- Test.js
+++ Test.js
@@ -1,20 +1,47 @@
+const http = require('http');
+const { url } = require('inspector');
+
 let i = 0;
+
 do
 {
-  const axios = require('axios');
-  
-  const promise = axios({
-    method: 'post',
-    url: 'http://127.0.0.1:8090/tasks',
-    data: {
-      firstName: 'Fred',
-      id: i,
-      lastName: 'Flintstone'
+  const req = http.request({
+    method: 'POST',
+    http: '1.1',
+    port: 8090,
+    path: '/tasks',
+    headers: 
+    {
+      'Content-length': '48',
+      'Content-type': 'application/json',
+      'Connection': 'close',
+      'Host': '127.0.0.1:8090',
+      'accept': '*/*\r\n\r\n',
+    },
+    
+    body:
+    {
+      'name:': 'Dmitry',
+      'id:': '0',
+      'lastname:': 'Ivanov'
     }
   });
-  
-  i++
 
+    req.on('response', function(response){
+      let body = '';
+
+      response.on('data', function(chunk){
+        body += chunk;
+      });
+
+      response.on('end', function(){
+        console.info(body);
+      });
+    });
+
+    req.end();
+
+    i++
 }
 while(i != 1000);
 
--- sample.js
+++ sample.js
@@ -3,19 +3,22 @@ const start = new Date();
 const axios = require('axios');
 
 const promise = axios({
-    method: 'get',
-    url: 'http://127.0.0.1:8090/tasks?id=9999'
+  method: 'get',
+  url: 'http://127.0.0.1:8090/tasks?id=999'
 });
 
 promise.then(processResponse);
+
+function processResponse(response) 
+{
   
-function processResponse(response) {
   console.log(response);
+
+  const end = new Date();
+  
+  const result = end - start;
+  
+  console.log(result);
 } 
 
-const end = new Date();
-
-const result = end - start;
-
-console.log(result);
 
